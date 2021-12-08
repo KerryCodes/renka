e9147ac630f7f8c5580543acdc1562f7643fca34
kerry <chengkaiquan@aidigger.com>
2021-12-08 15:55:02 +0800

    update

--- example/main.tsx
+++ example/main.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< HEAD:example/main.tsx
 import App from './app'
 import { createRenkaState } from '../src'
 import { initState } from './app/state'
-=======
-import './index.css'
-import App from './App'
-import { createRenkaState } from '../src'
-import { initState } from './state'
->>>>>>> 27d9fccab76d10581d1859986db6f891b3e615ac:test/main.tsx
 
 
 createRenkaState(initState)
